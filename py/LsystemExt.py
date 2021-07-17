c8635b1f965842595d8c8b8e971e02098f0a5bbf
yuno <yuno@av-controls.com>
2021-07-17 14:10:20 -0400

    update 4dan json export

--- py/LsystemExt.py
+++ py/LsystemExt.py
@@ -7,28 +7,34 @@ class LsystemExt:
 		# Operators
 		self.ownerComp = ownerComp
 		self.null_instance = ownerComp.op('null_instance')
+		self.geo_lsystem_1 = ownerComp.op('geo_lsystem_1')
+		self.geo_lsystem_2 = ownerComp.op('geo_lsystem_2')
+		self.geo_lsystem_3 = ownerComp.op('geo_lsystem_3')
+		self.geo_lsystem_4 = ownerComp.op('geo_lsystem_4')
+		self.instanceArray = [self.geo_lsystem_1, self.geo_lsystem_2, self.geo_lsystem_3, self.geo_lsystem_4]
 
-		# attributes:
-		self.a = 0 # attribute
-		self.B = 1 # promoted attribute
-
-		# initializations
+		# attributes
 		self.samples = None
+		self.rootDictionary = None
+		self.Generations = 3
+		self.Type = 'skel'
+
 
 	def ExportJSON(self):
 
+
+		self.rootDictionary = {}
 		self.samples = []
 
-		for i in range(0, self.null_instance.numSamples):
+		for sample in range(0, self.null_instance.numSamples):
 		
 			instanceData = {}
 			
-			for c in self.null_instance.chans():
+			for channel in self.null_instance.chans():
 				
-				instanceData[c.name] = self.null_instance[c.name][i]
+				instanceData[channel.name] = self.null_instance[channel.name][sample]
 			
 			self.samples.append(instanceData)
-	
 	
 		with open('json/instances.json', 'w') as outfile:
 			
