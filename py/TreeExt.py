3d4bb156dea07f2bb68e39cfabf53615781c2e2d
yuno <yuno@av-controls.com>
2021-07-26 22:07:09 -0400

    tree color

--- py/TreeExt.py
+++ py/TreeExt.py
@@ -9,7 +9,7 @@ class TreeExt:
 		# attributes:
 		self.NumDan = 3
 		self.NumTree = 12
-		self.DanDistance = 10
+		self.DanDistance = 5
 		self.DanDistanceOffset = 20
 		self.TruckHeight = 10
 		self.TruckPoint = 10
@@ -18,5 +18,6 @@ class TreeExt:
 	def RandomizeTreeShape(self):
 
 		for dan in range(4):
+			self.geo_trees.op('geo_dan_{}/base_shape/pattern_offsetX'.format(dan)).par.randomize.pulse()
 			self.geo_trees.op('geo_dan_{}/base_shape/pattern_offsetX'.format(dan)).par.randomize.pulse()
 			self.geo_trees.op('geo_dan_{}/base_shape/pattern_seed'.format(dan)).par.randomize.pulse()
