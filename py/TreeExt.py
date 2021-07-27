0e1bd72bd94faf68002e71bed275eb0006e4d4b9
yuno <yuno@av-controls.com>
2021-07-26 21:48:02 -0400

    4 trees

--- py/TreeExt.py
+++ py/TreeExt.py
@@ -4,17 +4,21 @@ class TreeExt:
 		
 		# Operators
 		self.ownerComp = ownerComp
+		self.geo_tree = self.ownerComp.op('geo_tree')
 
 		# attributes:
 		self.NumDan = 3
 		self.NumTree = 12
 		self.DanDistance = 10
+		self.DanDistanceOffset = 20
 		self.TruckHeight = 10
 		self.TruckPoint = 10
 
 
-	def myFunction(self, v):
-		debug(v)
+	def RandomizeTreeOffset(self):
 
-	def PromotedFunction(self, v):
-		debug(v)
+		# op('base_offset/pattern_offsetX').par.randomize.pulse()
+		# op('base_offset/pattern_offsetY').par.randomize.pulse()
+
+		for dan in range(3):
+			self.geo_tree.op('geo_dan_{}/base_offset/pattern_offsetX'.format(dan)).par.randomize.pulse()
