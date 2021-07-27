d78aca0dfd7e36fad3dbd5cadea915975d48982f
yuno <yuno@av-controls.com>
2021-07-26 22:19:25 -0400

    random position rule

--- py/TreeExt.py
+++ py/TreeExt.py
@@ -10,7 +10,7 @@ class TreeExt:
 		self.NumDan = 3
 		self.NumTree = 12
 		self.DanDistance = 5
-		self.DanDistanceOffset = 20
+		self.DanOffsetRange = 2
 		self.TruckHeight = 10
 		self.TruckPoint = 10
 
@@ -20,4 +20,5 @@ class TreeExt:
 		for dan in range(4):
 			self.geo_trees.op('geo_dan_{}/base_shape/pattern_offsetX'.format(dan)).par.randomize.pulse()
 			self.geo_trees.op('geo_dan_{}/base_shape/pattern_offsetX'.format(dan)).par.randomize.pulse()
-			self.geo_trees.op('geo_dan_{}/base_shape/pattern_seed'.format(dan)).par.randomize.pulse()
+			self.geo_trees.op('geo_dan_{}/base_shape/pattern_seed_offset'.format(dan)).par.randomize.pulse()
+			self.geo_trees.op('geo_dan_{}/base_shape/pattern_seed_truck'.format(dan)).par.randomize.pulse()
