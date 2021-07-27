d07b7af955b7119bca025b50326baccf047c0fff
yuno <yuno@av-controls.com>
2021-07-26 21:00:32 -0400

    create 1 truck

---
+++ py/TreeExt.py
@@ -0,0 +1,17 @@
+class TreeExt:
+	
+	def __init__(self, ownerComp):
+		
+		# Operators
+		self.ownerComp = ownerComp
+
+		# attributes:
+		self.numDan = 3
+		self.DanDistance = 10
+
+
+	def myFunction(self, v):
+		debug(v)
+
+	def PromotedFunction(self, v):
+		debug(v)
