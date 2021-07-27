5d76513a823d9c7e2823f3cef92a0e28ac098406
yuno <yuno@av-controls.com>
2021-07-26 22:03:05 -0400

    RandomizeTreeShape

--- py/TreeExt.py
+++ py/TreeExt.py
@@ -4,7 +4,7 @@ class TreeExt:
 		
 		# Operators
 		self.ownerComp = ownerComp
-		self.geo_tree = self.ownerComp.op('geo_tree')
+		self.geo_trees = self.ownerComp.op('geo_trees')
 
 		# attributes:
 		self.NumDan = 3
@@ -15,10 +15,8 @@ class TreeExt:
 		self.TruckPoint = 10
 
 
-	def RandomizeTreeOffset(self):
+	def RandomizeTreeShape(self):
 
-		# op('base_offset/pattern_offsetX').par.randomize.pulse()
-		# op('base_offset/pattern_offsetY').par.randomize.pulse()
-
-		for dan in range(3):
-			self.geo_tree.op('geo_dan_{}/base_offset/pattern_offsetX'.format(dan)).par.randomize.pulse()
+		for dan in range(4):
+			self.geo_trees.op('geo_dan_{}/base_shape/pattern_offsetX'.format(dan)).par.randomize.pulse()
+			self.geo_trees.op('geo_dan_{}/base_shape/pattern_seed'.format(dan)).par.randomize.pulse()
