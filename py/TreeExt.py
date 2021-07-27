05ad21065374cd05b9a91777dc4183fc57366d4d
yuno <yuno@av-controls.com>
2021-07-27 10:50:51 -0400

    fix randomize error, truck -> trunk

--- py/TreeExt.py
+++ py/TreeExt.py
@@ -4,46 +4,41 @@ class TreeExt:
 		
 		# Operators
 		self.ownerComp = ownerComp
-		self.geo_trees = self.ownerComp.op('geo_trees')
 
 		# attributes:
-		self.NumDan = 5
-		self.DanDistance = 4
+		self.NumDan = 7
+		self.DanDistance = 3
 		self.DanOffsetRangePosition = 2
-		self.DanOffsetRangeHeight = 5
-		self.DanOffsetRangeGravity = 20
+		self.DanOffsetRangeHeight = 2
+		self.DanOffsetRangeGravity = 5
 		self.TruckHeight = 8
-		self.TruckPoint = 10
+		self.TruckPoint = 5
 
 
-	
 	def CreateTrees(self):
 
-		self.geo_trees.op('geo_dan_0/replicator_tree').par.recreateall.pulse()
-		self.geo_trees.op('geo_dan_1/replicator_tree').par.recreateall.pulse()
-		self.geo_trees.op('geo_dan_2/replicator_tree').par.recreateall.pulse()
-		self.geo_trees.op('geo_dan_3/replicator_tree').par.recreateall.pulse()
-		
-
+		self.ownerComp.op('base_trees/geo_dan_0/replicator_tree').par.recreateall.pulse()
+		self.ownerComp.op('base_trees/geo_dan_1/replicator_tree').par.recreateall.pulse()
+		self.ownerComp.op('base_trees/geo_dan_2/replicator_tree').par.recreateall.pulse()
+		self.ownerComp.op('base_trees/geo_dan_3/replicator_tree').par.recreateall.pulse()
+	
 
 	def RandomizeTreeShape(self):
 
-		self.geo_trees.op('geo_dan_0/base_shape/pattern_seed_offset').par.randomize.pulse()
-		self.geo_trees.op('geo_dan_0/base_shape/pattern_seed_truck').par.randomize.pulse()
-		self.geo_trees.op('geo_dan_0/base_shape/pattern_seed_lsystem').par.randomize.pulse()
+		self.ownerComp.op('base_trees/geo_dan_0/base_shape/pattern_seed_offset').par.randomize.pulse()
+		self.ownerComp.op('base_trees/geo_dan_0/base_shape/pattern_seed_trunk').par.randomize.pulse()
+		self.ownerComp.op('base_trees/geo_dan_0/base_shape/pattern_seed_lsystem').par.randomize.pulse()
 
-		self.geo_trees.op('geo_dan_1/base_shape/pattern_seed_offset').par.randomize.pulse()
-		self.geo_trees.op('geo_dan_1/base_shape/pattern_seed_truck').par.randomize.pulse()
-		self.geo_trees.op('geo_dan_1/base_shape/pattern_seed_lsystem').par.randomize.pulse()
+		self.ownerComp.op('base_trees/geo_dan_1/base_shape/pattern_seed_offset').par.randomize.pulse()
+		self.ownerComp.op('base_trees/geo_dan_1/base_shape/pattern_seed_trunk').par.randomize.pulse()
+		self.ownerComp.op('base_trees/geo_dan_1/base_shape/pattern_seed_lsystem').par.randomize.pulse()
 
-		self.geo_trees.op('geo_dan_2/base_shape/pattern_seed_offset').par.randomize.pulse()
-		self.geo_trees.op('geo_dan_2/base_shape/pattern_seed_truck').par.randomize.pulse()
-		self.geo_trees.op('geo_dan_2/base_shape/pattern_seed_lsystem').par.randomize.pulse()
+		self.ownerComp.op('base_trees/geo_dan_2/base_shape/pattern_seed_offset').par.randomize.pulse()
+		self.ownerComp.op('base_trees/geo_dan_2/base_shape/pattern_seed_trunk').par.randomize.pulse()
+		self.ownerComp.op('base_trees/geo_dan_2/base_shape/pattern_seed_lsystem').par.randomize.pulse()
 
-		self.geo_trees.op('geo_dan_3/base_shape/pattern_seed_offset').par.randomize.pulse()
-		self.geo_trees.op('geo_dan_3/base_shape/pattern_seed_truck').par.randomize.pulse()
-		self.geo_trees.op('geo_dan_3/base_shape/pattern_seed_lsystem').par.randomize.pulse()
-
-		return
+		self.ownerComp.op('base_trees/geo_dan_3/base_shape/pattern_seed_offset').par.randomize.pulse()
+		self.ownerComp.op('base_trees/geo_dan_3/base_shape/pattern_seed_trunk').par.randomize.pulse()
+		self.ownerComp.op('base_trees/geo_dan_3/base_shape/pattern_seed_lsystem').par.randomize.pulse()
 
 		
